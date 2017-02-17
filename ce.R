f4a766d53c5863d83b770bc513a53ac686c30314
Ilario Gelmetti <ilario@eigenlab.org>
2017-02-17 11:03:45 +0100

    Added CE monoexponential fit

--- ce.R
+++ ce.R
@@ -1,6 +1,7 @@
 #library(robustbase)
 ce <- function(cedir="ce")
 {
+require(minpack.lm)
 print("CE: INTEGRATING")
 files <- list.files(path=cedir, pattern="^CE.*\\.txt.table$");
 mydata <- lapply(file.path(cedir,files), read.table, header=FALSE, col.names=c("time","voltage"));
@@ -8,11 +9,12 @@ files <- sub(".txt.table","",files);
 names(mydata) <- files;
 write.table(t(c("Voc","ChargeDensityCE")), file=file.path(cedir,"outputChargeDensityCE.txt"), append=FALSE, col.names=F, row.names=F);
 
+
 trashfornullmessages <- lapply(files, function(x) {
 	message(x);
 #	if(!file.exists(paste(x, ".png", sep=""))){
 	len<-length(mydata[[x]]$voltage)
-	endingvoltage <- mean(mydata[[x]]$voltage[(len*0.6):len])
+	endingvoltage <- mean(mydata[[x]]$voltage[(len*0.9):len]) #could be just zero
 	message(endingvoltage)
 	voltage2 <- mydata[[x]]$voltage - endingvoltage#, newdata = data.frame(mydata[[x]]$time))
 	current <- voltage2/50
@@ -28,9 +30,16 @@ trashfornullmessages <- lapply(files, function(x) {
         outputChargeDensityCE <- t(c(d, abs(totalchargedensity)));
 	write.table(outputChargeDensityCE, file=file.path(cedir,"outputChargeDensityCE.txt"), append=TRUE, col.names=F, row.names=F, quote=F);
 
+	decay <- mydata[[x]][mydata[[x]]$time>0,]
+
+
 	png(file.path(cedir,paste(x, ".png", sep="")), width=1280, heigh=800)
 	par(mar=c(5,4,4,5)+.1)
 	plot(mydata[[x]],type="l", ylab="Voltage (V)", xlab="Time (s)", main=paste(x,"CE"))
+tryCatch({
+expfit <- nlsLM(voltage~ C*exp(D*time), start=list(C=0.5*max(decay$voltage),D=-0.1*tail(decay$time, n=1)), data=decay)
+	lines(decay$time, predict(expfit), lwd=2, col="blue")
+}, error=function(e) print("Failed monoexponential fit"))
 	abline(h=endingvoltage, col="green")
 	par(new=TRUE)
 	plot(mydata[[x]]$time,charge/0.09, type="l", col="red", xaxt="n",yaxt="n",xlab="",ylab="")
