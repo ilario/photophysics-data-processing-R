65bc9a5f5193021201781c5b6c83d7e43a2d645e
Ilario Gelmetti <iochesonome@gmail.com>
2017-05-25 17:47:12 +0200

    statistics - include outliers in ylim, support comments column

--- iv-generate_statistics.R
+++ iv-generate_statistics.R
@@ -19,7 +19,7 @@ list2=list(`` = v2g1, `` = v2g2)
 #list3=list(`` = v3g1, `` = v3g2)
 
 
-a <- read.table("table", col.names = c("device","diode","reverse","Jsc","Voc","FF","PCE","notes"),fill = TRUE, na.strings=c("","NA"))
+a <- read.table("table", col.names = c("device","diode","reverse","Jsc","Voc","FF","PCE","comments","notes"),fill = TRUE, na.strings=c("","NA"))
 levels(a$notes) <- c(levels(a$notes), "NANA")
 a[is.na(a)] <- "NANA"
 
@@ -54,37 +54,42 @@ add.alpha <- function(col, alpha=1){
 			rgb(x[1], x[2], x[3], alpha=alpha))
 }
 
+signif.mean <- function(values, digits){
+	signif(mean(values), digits=digits)
+}
+
 single <- function(x,mar,mgp.y,mgp.x){
 	png(paste(name, "-", x, ".png", sep=""));
 	par(mar=mar)
-	boxplot(PCE~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(PCE~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$PCE),max(d$PCE)))
 	stripchart(PCE ~ droplevels(interaction(rev,get(x))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab="PCE (%)", mgp=mgp.y, cex.lab=1.5)
-	title(xlab=x, mgp=mgp.x, cex.lab=1.5)
+	title(xlab=gsub("_", " ", x), mgp=mgp.x, cex.lab=1.5)
+	print(aggregate(d$PCE, by=list(interaction(d$rev, d[[x]])), FUN=signif.mean, digits=4))
 	graphics.off()
 
 	png(paste(name, "-", x, "-Jsc.png", sep=""));
 	par(mar=mar)
-	boxplot(Jsc~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(Jsc~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$Jsc),max(d$Jsc)))
 	stripchart(Jsc ~ droplevels(interaction(rev,get(x))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab=bquote("J"["sc"]~"(mA/cm"^"2"*")"), mgp=mgp.y, cex.lab=1.5)
-	title(xlab=x, mgp=mgp.x, cex.lab=1.5)
+	title(xlab=gsub("_", " ", x), mgp=mgp.x, cex.lab=1.5)
 	graphics.off()
 
 	png(paste(name, "-", x, "-Voc.png", sep=""));
 	par(mar=mar)
-	boxplot(Voc~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(Voc~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$Voc),max(d$Voc)))
 	stripchart(Voc ~ droplevels(interaction(rev,get(x))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab=bquote("V"["oc"]~"(V)"), mgp=mgp.y, cex.lab=1.5)
-	title(xlab=x, mgp=mgp.x, cex.lab=1.5)
+	title(xlab=gsub("_", " ", x), mgp=mgp.x, cex.lab=1.5)
 	graphics.off()
 
 	png(paste(name, "-", x, "-FF.png", sep=""));
 	par(mar=mar)
-	boxplot(FF~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(FF~droplevels(interaction(rev,get(x),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$FF),max(d$FF)))
 	stripchart(FF ~ droplevels(interaction(rev,get(x))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab="FF", mgp=mgp.y, cex.lab=1.5)
-	title(xlab=x, mgp=mgp.x, cex.lab=1.5)
+	title(xlab=gsub("_", " ", x), mgp=mgp.x, cex.lab=1.5)
 	graphics.off()
 }
 
@@ -95,7 +100,7 @@ if(exists("var3")){single(x=var3, mar=c(12,6,1,1), mgp.y=c(4,1,0), mgp.x=c(8,1,0
 double <- function(x,y,mar,mgp.y,mgp.x){
 	png(paste(name, "-", x, "-", y, ".png", sep=""));
 	par(mar=mar)
-	boxplot(PCE~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(PCE~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$PCE),max(d$PCE)))
 	stripchart(PCE ~ droplevels(interaction(rev,get(x),get(y))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab="PCE (%)", mgp=mgp.y, cex.lab=1.5)
 	title(xlab=paste(x,y), mgp=mgp.x, cex.lab=1.5)
@@ -103,7 +108,7 @@ double <- function(x,y,mar,mgp.y,mgp.x){
 
 	png(paste(name,  "-", x,"-", y, "-Jsc.png", sep=""));
 	par(mar=mar)
-	boxplot(Jsc~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(Jsc~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$Jsc),max(d$Jsc)))
 	stripchart(Jsc ~ droplevels(interaction(rev,get(x),get(y))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab=bquote("J"["sc"]~"(mA/cm"^"2"*")"), mgp=mgp.y, cex.lab=1.5)
 	title(xlab=paste(x,y), mgp=mgp.x, cex.lab=1.5)
@@ -111,7 +116,7 @@ double <- function(x,y,mar,mgp.y,mgp.x){
 
 	png(paste(name,  "-", x,"-", y, "-Voc.png", sep=""));
 	par(mar=mar)
-	boxplot(Voc~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(Voc~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$Voc),max(d$Voc)))
 	stripchart(Voc ~ droplevels(interaction(rev,get(x),get(y))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab=bquote("V"["oc"]~"(V)"), mgp=mgp.y, cex.lab=1.5)
 	title(xlab=paste(x,y), mgp=mgp.x, cex.lab=1.5)
@@ -119,7 +124,7 @@ double <- function(x,y,mar,mgp.y,mgp.x){
 
 	png(paste(name,  "-", x,"-", y, "-FF.png", sep=""));
 	par(mar=mar)
-	boxplot(FF~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE)
+	boxplot(FF~droplevels(interaction(rev,get(x),get(y),sep="  ")), d, cex.axis=1.5, border=colors, las=2, outline=FALSE, ylim=c(min(d$FF),max(d$FF)))
 	stripchart(FF ~ droplevels(interaction(rev,get(x),get(y))), vertical = TRUE, data = d, method = "jitter", add = TRUE, pch = 20, col = add.alpha(colors,0.3), cex=2)
 	title(ylab="FF", mgp=mgp.y, cex.lab=1.5)
 	title(xlab=paste(x,y), mgp=mgp.x, cex.lab=1.5)
