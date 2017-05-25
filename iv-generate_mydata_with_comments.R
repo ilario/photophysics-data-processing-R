3989a9fcad747dc9939979d11accc884a4a04c17
Ilario Gelmetti <iochesonome@gmail.com>
2017-05-25 17:46:08 +0200

    generate mydata comments - better organization of code

--- iv-generate_mydata_with_comments.R
+++ iv-generate_mydata_with_comments.R
@@ -1,23 +1,31 @@
-v1g1=c("")
-v1g2=c("")
-v1g3=c("")
-v1all=c(v1g1, v1g2, v1g3)
+#name="ig"
 
-v2g1=c("")
-v2g2=v1all[! v1all %in% v2g1]
-v2all=v1all
+var1=""
+v1g1=c()
+v1g2=c()
+v1all=c(v1g1, v1g2)
+list1=list(`` = v1g1, `` = v1g2)
+
+var2=""
+v2g1=c()
+v2g2=c()#v1all[! v1all %in% v2g1]
+v2all=c(v2g1, v2g2)
+list2=list(`` = v2g1, `` = v2g2)
 
 file.create("output.txt")
-mydata <- import.iv.separated(pattern.excl="\\.png$", pattern="ig.*.txt$", list.excl="output.txt")
+mydata <- import.iv.separated(pattern.excl="\\.png$", pattern="^ig.*.txt$", list.excl="output.txt")
 results <- lapply(names(mydata), function(x){
 z <- factor(strsplit(x,"-")[[1]][2], v1all); 
-levels(z) <- list(`` = v1g1, `` = v1g2, `` = v1g3);
-#y <- factor(strsplit(x,"-")[[1]][2], v2all);
-#levels(y) <- list(`` = v2g1, `` = v2g2);
-y <- ""
+levels(z) <- list1;
+if(exists("var2")){
+	y <- factor(strsplit(x,"-")[[1]][2], v2all);
+	levels(y) <- list2;
+	comment=paste(toString(y), toString(z))}
+else {comment=toString(z)}
 
-write(extract.iv(mydata[[x]]$Voltage_V, mydata[[x]]$Current_mA, formatted.output=TRUE, directory=getwd(), sample=x, reverse=as.integer(grepl("reverse", x)), comment=paste(toString(y), toString(z))), file="output.txt", append=TRUE);
-extract.iv(mydata[[x]]$Voltage_V, mydata[[x]]$Current_mA, comment=paste(toString(y), toString(z)))});
+
+write(extract.iv(mydata[[x]]$Voltage_V, mydata[[x]]$Current_mA, formatted.output=TRUE, directory=getwd(), sample=x, reverse=as.integer(grepl("reverse", x)), comment=comment), file="output.txt", append=TRUE);
+extract.iv(mydata[[x]]$Voltage_V, mydata[[x]]$Current_mA, comment=comment)});
 names(results) <- names(mydata)
 
 
