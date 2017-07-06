36afc5cca0c88527ea5f53eefe1c806c090d86f8
Ilario Gelmetti <iochesonome@gmail.com>
2017-07-06 11:06:05 +0200

    Separe devices identification from statistics and commented output scripts

--- iv-generate_statistics.R
+++ iv-generate_statistics.R
@@ -1,16 +1,16 @@
-name="ig"
+#name="ig"
 
-var1=""
-v1g1=c()
-v1g2=c()
-v1all=c(v1g1, v1g2)
-list1=list(`` = v1g1, `` = v1g2)
+#var1=""
+#v1g1=c()
+#v1g2=c()
+#v1all=c(v1g1, v1g2)
+#list1=list(`` = v1g1, `` = v1g2)
 
-var2=""
-v2g1=c()
-v2g2=c()#v1all[! v1all %in% v2g1]
-v2all=c(v2g1, v2g2)
-list2=list(`` = v2g1, `` = v2g2)
+#var2=""
+#v2g1=c()
+#v2g2=c()#v1all[! v1all %in% v2g1]
+#v2all=c(v2g1, v2g2)
+#list2=list(`` = v2g1, `` = v2g2)
 
 #var3=""
 #v3g1=c()
