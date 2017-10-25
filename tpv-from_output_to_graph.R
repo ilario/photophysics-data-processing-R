4689824c915cfef4f7889a5420df7eb3265c77fb
Ilario Gelmetti <iochesonome@gmail.com>
2017-10-25 14:03:53 +0100

    tpv-from_output_to_graph added draft for plotting y0

--- tpv-from_output_to_graph.R
+++ tpv-from_output_to_graph.R
@@ -35,18 +35,6 @@ points(tpv$Voc, tpv$T2, pch=20, col="black")#, cex=4*tpv$A2/(tpv$A1+tpv$A2));
 graphics.off()
 
 
-#print("robust biexp")
-#
-#fulloutput <- read.table(file.path(tpvdir,"output-robustbiexp.txt"), header=TRUE);
-#n<-tail(grep("file",fulloutput[,1]),n=1)
-#tpv <- read.table(file.path(tpvdir,"output-robustbiexp.txt"), header=TRUE, skip=ifelse(length(n),n,0)); 
-#png(file.path(tpvdir, paste(name, "-tpv-robustbiexp.png",sep="")), width=1280, height=800);
-#plot(1, ylim=c(min(tpv$T1,tpv$T2),max(tpv$T2,tpv$T1)), xlim=c(min(tpv$Voc),max(tpv$Voc)), log="y", xlab="Voc (V)", ylab="Life-time (s)", main=paste(name, "TPV robust biexp"));
-#errbar(tpv$Voc, tpv$T1, tpv$T1+tpv$T1.error, tpv$T1-tpv$T1.error,log="y",add=TRUE)
-#errbar(tpv$Voc, tpv$T2, tpv$T2+tpv$T2.error, tpv$T2-tpv$T2.error,log="y",add=TRUE)
-#points(tpv$Voc, tpv$T1, pch=21, col="red", bg="red");
-#points(tpv$Voc, tpv$T2, col="red");
-#graphics.off()
 
 
 print("monoexp")
@@ -75,15 +63,7 @@ write.table(t(c("Tau0","beta")), file=file.path(tpvdir,"output-monoexp-fit.txt")
 output <- t(c(coef(fit)[[1]], coef(fit)[[2]]))
 write.table(output, file=file.path(tpvdir,"output-monoexp-fit.txt"), append=TRUE, col.names=F, row.names=F)
 
-#print("robust monoexp")
-#
-#fulloutput <- read.table(file.path(tpvdir,"output-robustmonoexp.txt"), header=TRUE);
-#n<-tail(grep("file",fulloutput[,1]),n=1)
-#tpv <- read.table(file.path(tpvdir,"output-robustmonoexp.txt"), header=TRUE, skip=ifelse(length(n),n,0)); 
-#png(file.path(tpvdir, paste(name, "-tpv-robustmonoexp.png",sep="")), width=1280, height=800);
-#plot(tpv$Voc, tpv$T, col="red", log="y", xlab="Voc (V)", ylab="Life-time (s)", main=paste(name, "TPV robust monoexp"));
-#errbar(tpv$Voc, tpv$T, tpv$T+tpv$T.error, tpv$T-tpv$T.error,log="y",add=TRUE)
-#graphics.off()
+
 
 if(file.exists(file.path(tpvdir, "output-mixedbimono.txt")))
 {
@@ -95,8 +75,15 @@ png(file.path(tpvdir, paste(name, "-tpv-mixedbimono.png",sep="")), width=600, he
 plot(1, ylim=c(min(tpv$T1,tpv$T2[!is.na(tpv$T2)]),max(tpv$T2[!is.na(tpv$T2)],tpv$T1)), xlim=c(min(tpv$Voc),max(tpv$Voc)), log="y", xlab="Voc (V)", ylab="Life-time (s)", main=paste(name, "TPV biexp and monoexp"), cex.axis=1.5, cex.lab=1.5);
 #errbar(tpv$Voc, tpv$T1, tpv$T1+tpv$T1.error, tpv$T1-tpv$T1.error,log="y",add=TRUE, pch="")
 #errbar(tpv$Voc, tpv$T2, tpv$T2+tpv$T2.error, tpv$T2-tpv$T2.error,log="y",add=TRUE, pch="")
-points(tpv$Voc, tpv$T1, pch=20, col="red")#, cex=4*tpv$A1/(tpv$A1+tpv$A2));
-points(tpv$Voc, tpv$T2, pch=20, col="black")#, cex=4*tpv$A2/(tpv$A1+tpv$A2));
+points(tpv$Voc, tpv$T1, pch=20, col="blue")#, cex=4*tpv$A1/(tpv$A1+tpv$A2));
+points(tpv$Voc, tpv$T2, pch=1, col="blue")#, cex=4*tpv$A2/(tpv$A1+tpv$A2));
+
+par(new = TRUE)
+plot(1, ylim=c(min(tpv$A1,tpv$A2[!is.na(tpv$A2)]),max(tpv$A2[!is.na(tpv$A2)],tpv$A1)), xlim=c(min(tpv$Voc),max(tpv$Voc)), axes = FALSE, bty = "n", xlab = "", ylab = "")
+points(tpv$Voc, tpv$A1, pch=20, col="red")
+points(tpv$Voc, tpv$A2, pch=1, col="red")
+axis(side=4, at = pretty(range(c(tpv$A1, tpv$A2))))
+mtext("y0", side=4, line=3)
 graphics.off()
 }
 }
