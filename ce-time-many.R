c320dc8b9bcb644f45fccd1fc8b06edfaf4b8043
Ilario Gelmetti <iochesonome@gmail.com>
2018-03-26 13:14:03 +0200

    new ce-time-many.R for plotting CE decay comparisons at 1 sun

---
+++ ce-time-many.R
@@ -0,0 +1,60 @@
+# Copyright (C) 2015-2016 Ilario Gelmetti <iochesonome@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#name=""
+
+title=gsub("_"," ",tail(unlist(strsplit(name,"-")),1))
+filename=gsub(",","",gsub(":","",name))
+
+library(RColorBrewer)
+library(robustbase)
+library(sfsmisc)
+library(Hmisc)
+require(minpack.lm)
+
+output=list()
+ce.max = 0
+
+i <- 0
+dirs <- list.dirs(recursive=FALSE)
+dirs <- sub("./","",dirs)
+legend=sub("-ig..-...-.","",sub("^0","",dirs))
+colors=colorRampPalette(c("red","orange","springgreen","royalblue"))(max(length(dirs),3))
+data <- lapply(dirs, function(x) {print(x);
+ce.files <- list.files(path=file.path(x, "ce"), pattern="^CE.*\\.txt.table$");
+ce.file <- tail(ce.files, n=1)
+print(ce.file)
+print(x)
+ce <- read.table(file.path(x, "ce", ce.file), header=F)
+ce.max <<- max(ce.max, max(ce$V2))
+ ce})
+names(data) <- dirs
+
+ylim=c(-0.05*ce.max, ce.max)
+xlim=c(-5e-8,2e-6)
+
+png(paste(filename, "-CE_times.png", sep=""), width=1000, heigh=1000)
+par(mar=c(5.1,5,2,2.1))
+plot(NULL, xlim=xlim, ylim=ylim, cex.main=1.5, xlab="Time (s)", ylab="Current (a.u.)", cex.lab=1.5, cex.axis=1.2, yaxt="n")
+lapply(dirs, function(x) {print(x);
+ lines(data[[x]]$V1, data[[x]]$V2, col=colors[i+1], lwd=2)
+ i <<- i+1
+})
+legend(x="topright",inset=0.05, legend, col=colors, cex=1.5, lwd=4, title=title, bg="gray90"
+)
+graphics.off()
+
+
+
