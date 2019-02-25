9b362820a458819991a73d3e7f6c87eeb0bf3e3b
Ilario Gelmetti <iochesonome@gmail.com>
2019-02-25 13:38:30 +0100

    iv-jsc_voc_vs_light_intensity improved and added slightly different plots due to alternative ideality factor definitions

--- iv-jsc_voc_vs_light_intensity.R
+++ iv-jsc_voc_vs_light_intensity.R
@@ -17,7 +17,9 @@ directory=tail(strsplit(getwd(), "/")[[1]], n=1)
 name=directory
 
 library(robustbase)
+library(minpack.lm)
 a <- read.table("output.txt",header=F,stringsAsFactors=F)
+names(a) = c("dir", "file", "reverse", "jsc", "voc", "ff", "pce")
 
 illumination <- function(names){
   values <- lapply(names, function(name){
@@ -29,44 +31,101 @@ illumination <- function(names){
   return(unlist(values))
 }
 
-a$suns <- illumination(a$V2)
+a$suns <- illumination(a$file)
 a<-a[with(a, order(a$suns)), ]
 
-fitJsc<-nlrob(V4 ~ B*suns^C, start=list(B=20, C=1), data=a)
+fitJsc<-nlrob(jsc ~ B*suns^C, start=list(B=20, C=1), data=a)
 alfa=signif(fitJsc$coefficients["C"],3)
 
 png(paste(name,"-Jsc_vs_LI.png",sep=""), width=640, height=640)
 par(mar=c(7,7,4.1,2.1))
-plot(a$suns,a$V4,cex.main=2,ylab=bquote("J"["sc"]~"(mA/cm"^"2"*")"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5)
+plot(a$suns,a$jsc,cex.main=2,ylab=bquote("J"["SC"]~"(mA/cm"^"2"*")"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5)
 lines(a$suns, predict(fitJsc))
-mtext(bquote("Jsc" == .(signif(fitJsc$coefficients["B"],3)) * " LI%" ^ .(alfa)),side=3,line=-3,cex=2)
+mtext(bquote("J"["SC"] == .(signif(fitJsc$coefficients["B"],3)) * " LI%" ^ .(alfa)),side=3,line=-3,cex=2)
 mtext(bquote(alpha == .(alfa)),side=3,line=-5,cex=2)
 graphics.off()
 
 b=a[a$suns != 0,]
 print(b)
-fitVoc<-lmrob(b$V5 ~ log(b$suns))
+fitVoc<-lmrob(b$voc ~ log(b$suns))
 
 nid <- fitVoc$coefficients[2]
 
 png(paste(name,"-Voc_vs_LI.png",sep=""), width=640, height=640)
 par(mar=c(5.5,5,4.1,2.1))
-plot(b$suns,b$V5,cex.main=2,ylab=bquote("V"["oc"]~"(V)"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5, log="x")
+plot(b$suns,b$voc,cex.main=2,ylab=bquote("V"["OC"]~"(V)"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5, log="x")
 lines(b$suns, predict(fitVoc))
-mtext(bquote("Voc" == .(signif(fitVoc$coefficients[1],3)) + .(signif(nid,3)) ~ "ln(LI%)"),side=3,line=-3,cex=2)
+mtext(bquote("V"["OC"] == .(signif(fitVoc$coefficients[1],3)) + .(signif(nid,3)) ~ "ln(LI%)"),side=3,line=-3,cex=2)
 mtext(bquote("n"["id"] == .(signif(nid/0.02585,3))),side=3,line=-5,cex=2)
 graphics.off()
 
-bReverse = b[grepl("reverse",b$V2),]
+bReverse = b[as.logical(b$reverse),]
 print(bReverse)
-fitVocReverse<-lmrob(bReverse$V5 ~ log(bReverse$suns))
+fitVocReverse<-lmrob(bReverse$voc ~ log(bReverse$suns))
 nidReverse <- fitVocReverse$coefficients[2]
 
 png(paste(name,"-VocReverse_vs_LI.png",sep=""), width=640, height=640)
 par(mar=c(5.5,5,4.1,2.1))
-plot(bReverse$suns,bReverse$V5,cex.main=2,ylab=bquote("V"["oc"]~"(V)"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5, log="x")
+plot(bReverse$suns,bReverse$voc,cex.main=2,ylab=bquote("V"["OC"]~"(V)"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5, log="x")
 lines(bReverse$suns, predict(fitVocReverse))
-mtext(bquote("Voc" == .(signif(fitVocReverse$coefficients[1],3)) + .(signif(nidReverse,3)) ~ "ln(LI%)"),side=3,line=-3,cex=2)
+mtext(bquote("V"["OC"] == .(signif(fitVocReverse$coefficients[1],3)) + .(signif(nidReverse,3)) ~ "ln(LI%)"),side=3,line=-3,cex=2)
 mtext(bquote("n"["id"] == .(signif(nidReverse/0.02585,3))),side=3,line=-5,cex=2)
 graphics.off()
 
+fitVoc2<-nlsLM(voc ~ A*log(suns/D + 1), start=list(A=3E-2, D=4E-10), data=b, lower=c(0,1E-30), algorithm="port")
+tryCatch({
+	fitVoc2<-nlrob(voc ~ A*log(suns/D + 1), start=coef(fitVoc2), data=b, lower=c(0,1E-30), algorithm="port")
+}, error=function(e) cat("Robust fit: Error ", e$message, "\n"))
+coef2 = coef(fitVoc2)
+
+png(paste(name,"-Voc_vs_LI2.png",sep=""), width=640, height=640)
+par(mar=c(5.5,5,4.1,2.1))
+plot(b$suns,b$voc,cex.main=2,ylab=bquote("V"["OC"]~"(V)"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5, log="x")
+lines(b$suns, predict(fitVoc2))
+mtext(bquote("V"["OC"] == .(signif(coef2[["A"]],3)) ~ "ln(LI% / "~.(signif(coef2[["D"]],3))~" + 1)"),side=3,line=-3,cex=2)
+mtext(bquote("n"["id"] == .(signif(coef2[["A"]]/0.02585,3))),side=3,line=-5,cex=2)
+graphics.off()
+
+fitVocReverse2 <- nlsLM(voc ~ A*log(suns/D + 1), start=list(A=4E-2, D=4E-10), data=bReverse, lower=c(0,1E-30), algorithm="port")
+tryCatch({
+	fitVocReverse2<-nlrob(voc ~ A*log(suns/D + 1), start=coef(fitVocReverse2), data=bReverse, lower=c(0,1E-30), algorithm="port")
+}, error=function(e) cat("Robust fit: Error ", e$message, "\n"))
+coefReverse2 <- coef(fitVocReverse2)
+print(coefReverse2)
+
+png(paste(name,"-VocReverse_vs_LI2.png",sep=""), width=640, height=640)
+par(mar=c(5.5,5,4.1,2.1))
+plot(bReverse$suns,bReverse$voc,cex.main=2,ylab=bquote("V"["OC"]~"(V)"),xlab="Light Intensity (%sun)", cex.lab=2,cex.axis=1.5, log="x")
+lines(bReverse$suns, predict(fitVocReverse2))
+mtext(bquote("V"["OC"] == .(signif(coefReverse2[["A"]],3)) ~ "ln(LI% / "~.(signif(coefReverse2[["D"]],3))~" + 1)"),side=3,line=-3,cex=2)
+mtext(bquote("n"["id"] == .(signif(coefReverse2[["A"]]/0.02585,3))),side=3,line=-5,cex=2)
+graphics.off()
+
+fitVoc3 <- nlsLM(voc ~ A*log(jsc/E + 1), start=list(A=4E-2, E=4E-10), data=b, lower=c(0,1E-30), algorithm="port")
+tryCatch({
+	fitVoc3<-nlrob(voc ~ A*log(jsc/E + 1), start=coef(fitVoc3), data=b, lower=c(0,1E-30), algorithm="port")
+}, error=function(e) cat("Robust fit: Error ", e$message, "\n"))
+coef3 <- coef(fitVoc3)
+print(coef3)
+
+png(paste(name,"-Voc_vs_LI3.png",sep=""), width=640, height=640)
+par(mar=c(5.5,5,4.1,2.1))
+plot(b$jsc,b$voc,cex.main=2,ylab=bquote("V"["OC"]~"(V)"),xlab=bquote("J"["SC"]~" (mA/cm"^"2"*")"), cex.lab=2,cex.axis=1.5, log="x")
+lines(b$jsc, predict(fitVoc3))
+mtext(bquote("V"["OC"] == .(signif(coef3[["A"]],3)) ~ "ln(J"["SC"]~" / "~.(signif(coef3[["E"]]))~" + 1)"),side=3,line=-3,cex=2)
+mtext(bquote("n"["id"] == .(signif(coef3[["A"]]/0.02585,3))),side=3,line=-5,cex=2)
+graphics.off()
+
+fitVocReverse3 <- nlsLM(voc ~ A*log(jsc/E + 1), start=list(A=4E-2, E=4E-10), data=bReverse, lower=c(0,1E-30), algorithm="port")
+tryCatch({
+	fitVocReverse3<-nlrob(voc ~ A*log(jsc/E + 1), start=coef(fitVocReverse3), data=bReverse, lower=c(0,1E-30), algorithm="port")
+}, error=function(e) cat("Robust fit: Error ", e$message, "\n"))
+coefReverse3 <- coef(fitVocReverse3)
+
+png(paste(name,"-VocReverse_vs_LI3.png",sep=""), width=640, height=640)
+par(mar=c(5.5,5,4.1,2.1))
+plot(bReverse$jsc,bReverse$voc,cex.main=2,ylab=bquote("V"["OC"]~"(V)"),xlab=bquote("J"["SC"]~" (mA/cm"^"2"*")"), cex.lab=2,cex.axis=1.5, log="x")
+lines(bReverse$jsc, predict(fitVocReverse3))
+mtext(bquote("V"["OC"] == .(signif(coefReverse3[["A"]],3)) ~ "ln(J"["SC"]~" / "~.(signif(coefReverse3[["E"]]))~" + 1)"),side=3,line=-3,cex=2)
+mtext(bquote("n"["id"] == .(signif(coefReverse3[["A"]]/0.02585,3))),side=3,line=-5,cex=2)
+graphics.off()
