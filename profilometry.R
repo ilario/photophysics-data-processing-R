b5191759aac18eaee7acaf2649ea84baf8d7d434
Ilario Gelmetti <iochesonome@gmail.com>
2019-02-27 13:09:43 +0100

    profilometry write text of multiple heights just if different enough

--- profilometry.R
+++ profilometry.R
@@ -22,10 +22,10 @@ lapply(files, function(x) {print(x);
 		png(paste(x,".png",sep=""), width=image_width, height=image_height)
 	}
 		par(mar = c(4,5,1,1) + 0.1) ## default is c(5,4,4,2) + 0.1
-	        plot(substrateToZero, type="l", ylab="Height (nm)", xlab="", xaxt="n", bty="n", ylim=c(0,min(1500,high*1.2)), lwd=2, cex.lab=1.7, cex.axis=1.4, panel.first=c(abline(h=0, col="gray80"), abline(h=mid, col="gray80"), abline(h=high, col="gray80")))
+	        plot(substrateToZero, type="l", ylab="Height (nm)", xlab="", xaxt="n", bty="n", ylim=c(0,min(1500,high*1.2)), lwd=2, cex.lab=1.7, cex.axis=1.4, panel.first=c(abline(h=0, col="gray80"), if(mid < 0.9*high){abline(h=mid, col="gray80")}, abline(h=high, col="gray80")))
         	mtext(side = 1, text = paste(length,"nm"), line=1, cex=1.7)
         	text(length*1000*0.9, high, paste(round(high),"nm"), col="red", cex=1.7, font=2)
-        	text(length*1000*0.9, mid, paste(round(mid),"nm"), col="red", cex=1.7, font=2)
+        	if(mid < 0.9*high){text(length*1000*0.9, mid, paste(round(mid),"nm"), col="red", cex=1.7, font=2)}
 		abline(h=0, col="gray50")
 	dev.off()
 
@@ -43,10 +43,10 @@ lapply(files, function(x) {print(x);
 		png(paste(x,"-flat.png",sep=""), width=image_width, height=image_height)
 	}
 		par(mar = c(4,5,1,1) + 0.1)
-	        plot(substrateToZeroSub, type="l", ylab="Height (nm)", xlab="", xaxt="n", bty="n", ylim=c(0,min(1500,highSub*1.2)), lwd=2, cex.lab=1.7, cex.axis=1.4, panel.first=c(abline(h=0, col="gray80"), abline(h=midSub, col="gray80"), abline(h=highSub, col="gray80")))
+	        plot(substrateToZeroSub, type="l", ylab="Height (nm)", xlab="", xaxt="n", bty="n", ylim=c(0,min(1500,highSub*1.2)), lwd=2, cex.lab=1.7, cex.axis=1.4, panel.first=c(abline(h=0, col="gray80"), if(midSub < 0.9*highSub){abline(h=midSub, col="gray80")}, abline(h=highSub, col="gray80")))
         	mtext(side = 1, text = paste(length,"nm"), line=1, cex=1.7)
         	text(length*1000*0.9, highSub, paste(round(highSub),"nm"), col="red", cex=1.7, font=2)
-        	text(length*1000*0.9, midSub, paste(round(midSub),"nm"), col="red", cex=1.7, font=2)
+        	if(midSub < 0.9*highSub){text(length*1000*0.9, midSub, paste(round(midSub),"nm"), col="red", cex=1.7, font=2)}
 		abline(h=0, col="gray50")
 	dev.off()
 
@@ -63,10 +63,10 @@ lapply(files, function(x) {print(x);
 		png(paste(x,"-flat2.png",sep=""), width=image_width, height=image_height)
 	}
 		par(mar = c(4,5,1,1) + 0.1)
-	        plot(substrateToZeroSur, type="l", ylab="Height (nm)", xlab="", xaxt="n", bty="n", ylim=c(0,min(1500,highSur*1.2)), lwd=2, cex.lab=1.7, cex.axis=1.4, panel.first=c(abline(h=0, col="gray80"), abline(h=midSur, col="gray80"), abline(h=highSur, col="gray80")))
+	        plot(substrateToZeroSur, type="l", ylab="Height (nm)", xlab="", xaxt="n", bty="n", ylim=c(0,min(1500,highSur*1.2)), lwd=2, cex.lab=1.7, cex.axis=1.4, panel.first=c(abline(h=0, col="gray80"), if(midSur < 0.9*highSur){abline(h=midSur, col="gray80")}, abline(h=highSur, col="gray80")))
         	mtext(side = 1, text = paste(length,"nm"), line=1, cex=1.7)
-        	text(length*1000*0.9, highSur, paste(round(highSur),"nm"), col="red", cex=1.7, font=2)
-        	text(length*1000*0.9, midSur, paste(round(midSur),"nm"), col="red", cex=1.7, font=2)
+        	text(length*1000*0.9, highSur, paste(round(highSur),"nm"), col="red", cex=2, font=2)
+		if(midSur < 0.9*highSur){text(length*1000*0.9, midSur, paste(round(midSur),"nm"), col="red", cex=2, font=2)}
 		abline(h=0, col="gray50")
 	dev.off()
 })
